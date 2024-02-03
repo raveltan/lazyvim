20307874fd337a2793d365de3ea3d4b46ad0b4fe
Ravel Tanjaya <s3937118@student.rmit.edu.au>
2024-02-03 12:36:33 +1100

    feat: added plugins

--- init.lua
+++ init.lua
@@ -1,2 +1,42 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+if vim.g.vscode then
+  -- VSCode extension
+  local vscode = require("vscode-neovim")
+  vim.api.nvim_set_keymap(
+    "n",
+    "<Space>ca",
+    ':lua require("vscode-neovim").action("editor.action.quickFix")<CR>',
+    { noremap = true, silent = true }
+  )
+  vim.api.nvim_set_keymap(
+    "n",
+    "<Space>e",
+    ':lua require("vscode-neovim").action("workbench.view.explorer")<CR>',
+    { noremap = true, silent = true }
+  )
+  vim.api.nvim_set_keymap(
+    "n",
+    "<Space>tt",
+    ':lua require("vscode-neovim").action("workbench.action.terminal.toggleTerminal")<CR>',
+    { noremap = true, silent = true }
+  )
+  vim.api.nvim_set_keymap(
+    "n",
+    "gr",
+    ':lua require("vscode-neovim").action("editor.action.referenceSearch.trigger")<CR>',
+    { noremap = true, silent = true }
+  )
+  vim.api.nvim_set_keymap(
+    "n",
+    "<Space>sg",
+    ':lua require("vscode-neovim").action("workbench.action.findInFiles")<CR>',
+    { noremap = true, silent = true }
+  )
+  vim.api.nvim_set_keymap(
+    "n",
+    "<Space>cr",
+    ':lua require("vscode-neovim").action("editor.action.rename")<CR>',
+    { noremap = true, silent = true }
+  )
+end
---
+++ lua/plugins/razor.lua
@@ -0,0 +1,5 @@
+return {
+  {
+    "jlcrochet/vim-razor",
+  },
+}
